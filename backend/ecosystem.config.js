c78605c8c81d89a1aff039273ae4d10ed036027f
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-05-30 14:58:54 +0330

    ecosystem thing

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,10 +3,10 @@ module.exports = {
   apps: [
     {
       name: 'hampa-backend',
-      script: './dist/main.js', // Point directly to the compiled JavaScript file
-      interpreter: '/home/hossein/.bun/bin/bun', // Specify the full path to Bun
+      script: '/var/www/hampa/backend/dist/main.js',
+      interpreter: '/home/hossein/.bun/bin/bun', // Confirm path with `which bun`
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork', // Use 'fork' for Bun compatibility
       watch: false,
       autorestart: true,
       max_memory_restart: '1G',
